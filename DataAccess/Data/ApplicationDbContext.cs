fda1332797f1d9163e77c1c6ed419b2f26009062
taheriara <h.taheriara@gmail.com>
2022-11-02 13:00:06 +0330

    Add project files.

---
+++ DataAccess/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using DataAccess.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccess.Data
+{
+	public class ApplicationDbContext : DbContext
+	{
+		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+			: base(options)
+		{ }
+
+		public DbSet<CoffeeShop> CoffeeShops { get; set; }
+	}
+}
